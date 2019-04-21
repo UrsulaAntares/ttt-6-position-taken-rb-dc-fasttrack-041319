66424959fafbddfb5ae71118a197d7e686a4b96d
Ursula Miller <ursula.miller+fi@gmail.com>
2019-04-21 19:21:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,20 @@
 # code your #position_taken? method here!
+
+
+board = [" ", " ", " "]
+
+user_input = gets.strip
+
+move = user_input.to_i
+
+
+
+
+def position_taken(board)
+  if board[2] = " "
+    return FALSE
+  else puts "That spot is taken, choose another"
+    
+  end
+  
+position_taken(board)
